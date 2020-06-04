f496e645cd755750740f93a541b4375d115f5f36
avalanche05 <57392204+avalanche05@users.noreply.github.com>
2020-06-04 17:46:49 +0300

    a

--- src/main/java/com/server/restaurantrent/controllers/MessageController.java
+++ src/main/java/com/server/restaurantrent/controllers/MessageController.java
@@ -1,13 +1,35 @@
 package com.server.restaurantrent.controllers;
 
 
+import com.server.restaurantrent.models.Message;
 import com.server.restaurantrent.repo.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class MessageController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @PostMapping("/message/send")
+    public boolean sendMessage(@RequestBody Message message){
+        messageRepository.save(message);
+        return true;
+    }
+    @PostMapping("/message/get")
+    public ArrayList<Message> getMessage(@RequestParam Long idRent){
+        ArrayList<Message> messages = new ArrayList<>();
+        for(Message temp : messageRepository.findAll()){
+            if(temp.getIdRent() == idRent){
+                messages.add(temp);
+            }
+        }
+        return messages;
+    }
 }
