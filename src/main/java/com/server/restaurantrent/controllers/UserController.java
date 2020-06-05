5b53ab01ad4eae5bf86879aabd55649594f65b99
avalanche05 <57392204+avalanche05@users.noreply.github.com>
2020-06-05 13:44:41 +0300

    a

--- src/main/java/com/server/restaurantrent/controllers/UserController.java
+++ src/main/java/com/server/restaurantrent/controllers/UserController.java
@@ -1,21 +1,29 @@
 package com.server.restaurantrent.controllers;
 
 
+import com.server.restaurantrent.models.ConfirmationToken;
 import com.server.restaurantrent.models.User;
+import com.server.restaurantrent.repo.ConfirmationTokenRepository;
 import com.server.restaurantrent.repo.UserRepository;
+import com.server.restaurantrent.services.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     @PostMapping("/user/signup")
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,6 +33,15 @@ public class UserController {
                 return new User();
             }
         }
+        ConfirmationToken confirmationToken = new ConfirmationToken(new User(email,password));
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(email);
+        mailMessage.setSubject("Complete Registration!");
+        mailMessage.setFrom("chand312902@gmail.com");
+        mailMessage.setText("To confirm your account, please click here : "
+                +"http://localhost:8082/confirm-account?token="+confirmationToken.getConfirmationToken());
+
+        emailSenderService.sendEmail(mailMessage);
         User user = new User(email,password);
         return userRepository.save(user);
     }
@@ -40,6 +57,8 @@ public class UserController {
         }
         return new User();
     }
+
+
 
 }
 
